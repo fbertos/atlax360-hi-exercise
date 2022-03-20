f9059fc97919c4750727c3e7d098ed012c09d0f7
Fernando Bertos Matilla <fbertos@gmail.com>
2022-03-20 13:08:43 +0100

    Python exercise for ATLAX 360 IT hiring processes

---
+++ src/libs/DBExtractor.py
@@ -0,0 +1,36 @@
+import json
+import pyodbc
+
+class DBExtractor():
+    def __init__(self, configFile: str):
+        f = open(configFile)
+        data = json.load(f)
+        
+        self._HOST = data["HOST"]
+        self._PORT = data["PORT"]
+        self._DATABASE = data["DATABASE"]
+        self._USER = data["USER"]
+        self._PASSWORD = data["PASSWORD"]
+        
+        f.close()
+ 
+
+    def extract(self, targetFile: str):
+        conn = None
+        
+        try:
+            conn = pyodbc.connect("DRIVER={ODBC Driver 18 for SQL Server}" +
+                                ";SERVER=" + self._HOST + 
+                                ";DATABASE=" + self._DATABASE + 
+                                ";UID=" + self._USER + 
+                                ";PWD=" + self._PASSWORD + 
+                                ";TrustServerCertificate=Yes")
+            
+            # Insert your exercise code here
+            #
+            #
+            # End of exercise
+        except:
+            print("error extracting data from sqlserver")
+        finally:        
+            if conn: conn.close()
